55c88bebba336a814f8841ba1b43da5199c6aa13
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2022-02-28 16:57:38 +0100

    char to int and int to char methods.

--- src/Decrypting/Decrypting.java
+++ src/Decrypting/Decrypting.java
@@ -1,4 +1,39 @@
 package Decrypting;
 
+import java.util.Scanner;
+
 public class Decrypting {
+
+  public static void main(String[] args) {
+    new Decrypting().go();
+  }
+
+  void go() {
+    intToChar();
+  }
+
+  void intToChar() {
+    Scanner in = new Scanner(System.in);
+    int charNumber = in.nextInt();
+
+    if ((charNumber >= 1 && charNumber <= 26) || charNumber >= 27 && charNumber <= 29) {
+      // a to z ascii = 1 to 26
+      if (charNumber >= 1 && charNumber <= 26) {
+        charNumber = charNumber + 64;
+        //Æ ascii = 27
+      } else if (charNumber == 27) {
+        charNumber = charNumber + 171;
+        //Ø ascii = 28
+      } else if (charNumber == 28) {
+        charNumber = charNumber + 188;
+        //Å ascii = 29
+      } else if (charNumber == 29) {
+        charNumber = charNumber + 168;
+      }
+      //charSpace
+    } else {
+      charNumber = 0;
+    }
+  }
 }
+
--- src/Encrypting/Encrypting.java
+++ src/Encrypting/Encrypting.java
@@ -1,4 +1,41 @@
 package Encrypting;
 
+import java.util.Scanner;
+
 public class Encrypting {
+
+  public static void main(String[] args) {
+    new Encrypting().go();
+  }
+
+  void go() {
+    charToInt();
+
+  }
+
+  void charToInt() {
+    Scanner in = new Scanner(System.in);
+    char charLetter = in.next().charAt(0);
+    char charSpace = ' ';
+    int asciiEncrypt = Character.toUpperCase(charLetter);
+
+    if ((asciiEncrypt >= 65 && asciiEncrypt <= 90) || asciiEncrypt >= 197 && asciiEncrypt <= 216) {
+      // a to z ascii = 1 to 26
+      if (asciiEncrypt >= 65 && asciiEncrypt <= 90) {
+        asciiEncrypt = asciiEncrypt - 64;
+        //Æ ascii = 27
+      } else if (asciiEncrypt == 198) {
+        asciiEncrypt = asciiEncrypt - 171;
+        //Ø ascii = 28
+      } else if (asciiEncrypt == 216) {
+        asciiEncrypt = asciiEncrypt - 188;
+        //Å ascii = 29
+      } else if (asciiEncrypt == 197) {
+        asciiEncrypt = asciiEncrypt - 168;
+      }
+      //charSpace
+    } else {
+      asciiEncrypt = 0;
+    }
+  }
 }
